7ddf397d55a18c1e1308fb71eb351a67d647e5e5
Sarah Battey <sarahsbattey@gmail.com>
2019-04-11 12:30:50 -0600

    add router config

---
+++ api/routes.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
+const path = ('path')
+
+const allShows = require('./data/tvShows')
+const allMovies = require('./data/movies')
+const config = require('./config')
+
+const router = express.Router()
+
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${config.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  audience: [config.AUDIENCE],
+  issuer: `https://${config.AUTH0_DOMAIN}/`,
+  algorithm: 'RS256'
+})
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
+router.get('/api/', (req, res) => {
+  res.send('API is working')
+})
+
+router.get('/api/data/tvshows', (req, res) => {
+  res.json(allShows)
+})
+
+router.get('/api/data/movies',authCheck, (req, res) => {
+  res.json(allMovies)
+})
+
+module.exports = router
